9a7adb1100b9b8b881a1f04098defb4e065e0f98
nconrad <1194246+nconrad@users.noreply.github.com>
2023-03-21 11:08:49 -0500

    add blog preview

--- src/components/home/BlogPreview.tsx
+++ src/components/home/BlogPreview.tsx
@@ -2,29 +2,58 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 
-type Props = {
 
-}
+/**
+ * BlogPreview parses the articles from production site (or whatever is provided in /docusaurus.config.js)
+ * and renders the article body (up until <!--truncate-->), including with links to articles
+ *
+ * Note docusaurus is following the schema.org format https://schema.org/BlogPosting
+ */
 
-export default function BlogPreview(props: Props) {
+export default function BlogPreview() {
 
   const [data, setData] = useState()
+  const {siteConfig} = useDocusaurusContext()
 
   useEffect(() => {
-    fetch(`/blog/blog-archive-80c.json`)
-      .then(res => res.json())
-      .then(data => setData(data.blogPosts))
+    // fetch blog preview from prod site
+    const {url, baseUrl} = siteConfig
+    fetch(url + baseUrl + 'blog')
+      .then(res => res.text())
+      .then(content => {
+        const parser = new DOMParser()
+        const parsedHtml = parser.parseFromString(content, 'text/html')
+        const items = parsedHtml.getElementsByTagName("article")
+
+        let data
+        try {
+          data = [...items].map(ele => {
+            const url = ele.querySelectorAll('[itemprop=url')[0].href
+            return {
+              headline: ele.querySelectorAll('[itemprop=headline')[0].innerText,
+              articleBody: ele.querySelectorAll('[itemprop=articleBody')[0].innerText,
+              url: url.slice(url.indexOf('/', 8)) // strip domain
+            }
+          })
+        } catch (e) {
+          console.log(`Could not parse ${url}`, e)
+        }
+
+        setData(data)
+      })
   }, [])
 
   return (
     <Root>
-      {(data || []).map(blog => {
-        const {title, description, permalink} = blog.metadata
+      {(data || []).map(meta => {
+        const {headline, articleBody, url} = meta
+
         return (
-          <div key={title}>
-            <h3><Link to={permalink}>{title}</Link></h3>
-            <p>{description} <Link to={permalink}>Read more...</Link></p>
+          <div key={headline}>
+            <h3><Link to={url}>{headline}</Link></h3>
+            <p>{articleBody} <Link to={url}>Read more...</Link></p>
           </div>
         )
       })}
