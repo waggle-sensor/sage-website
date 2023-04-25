e766bce625ee43b1825a4f6e919a8a93531e4573
nconrad <1194246+nconrad@users.noreply.github.com>
2023-04-25 16:17:36 -0500

    add linter config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': ['error','unix'],
+    'quotes': ['error', 'single', {'allowTemplateLiterals': true}],
+    'semi': ['error', 'never'],
+    'spaced-comment': ['error', 'always'],
+    'react/prop-types': 0,
+    '@typescript-eslint/no-unused-vars': 'error',
+    'react/no-unescaped-entities': [
+      'error',
+      {
+        forbid: ['>', '}'],
+      },
+    ],
+    '@typescript-eslint/no-var-requires': 'off'
+  },
+  'globals': {
+    'require': true,
+    'module': true,
+  }
+}
