eb4e37330e5b86747de8a3469e05a3bf135a16ef
nconrad <1194246+nconrad@users.noreply.github.com>
2023-05-09 14:08:53 -0500

    add notice banner

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,12 @@ module.exports = {
   trailingSlash: false,
   /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
   themeConfig: {
+    announcementBar: {
+      content: '<b>ℹ️ The Sage portal and services will be intermittently offline from Tuesday, May 9th to Friday, May 12th as Argonne performs upgrades to one of its data centers. ℹ️</b>',
+      isCloseable: false,
+      textColor: 'rgb(1, 67, 97)',
+      backgroundColor: 'rgb(229, 246, 253)'
+    },
     navbar: {
       title: 'Sage',
       logo: {
