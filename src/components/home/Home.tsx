0c4be0517efefa11a1adb8ef372efd97acb5992a
nconrad <1194246+nconrad@users.noreply.github.com>
2023-10-04 16:09:32 -0500

    add some icons; blog list title styling

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,7 +5,15 @@ import styled from 'styled-components'
 
 import Arrow from '@mui/icons-material/PlayCircleOutlineRounded'
 import LaunchRounded from '@mui/icons-material/LaunchRounded'
+import LearnIcon from '@mui/icons-material/SchoolOutlined'
+import ContributeIcon from '@mui/icons-material/Diversity1Outlined'
+import RunIcon from '@mui/icons-material/PendingActionsOutlined'
+import BrowseIcon from '@mui/icons-material/QueryStatsRounded'
+import AnalyzeIcon from '@mui/icons-material/BarChartOutlined'
 import NewsIcon from '@mui/icons-material/FeedOutlined'
+import EventsIcon from '@mui/icons-material/CalendarMonthOutlined'
+import DevIcon from '@mui/icons-material/CodeRounded'
+import SciIcon from '@mui/icons-material/ScienceOutlined'
 
 import CodeWindow from './CodeWindow'
 import NewsPreview from './NewsPreview'
@@ -132,27 +140,37 @@ export default function Home() {
           <div className="flex flex-col md:flex-row gap-10 md:gap-4 xl:gap-10">
             <Link to="science" className="card">
               <img src={require('@site/static/img/home/learn.jpg').default} />
-              <h3>Learn</h3>
+              <h3 className="flex items-center gap-2">
+                <LearnIcon />Learn
+              </h3>
               <p>Explore some of the <Link to="science">science</Link> made possible with Sage</p>
             </Link>
             <a href={`${portal}/apps`} className="card">
               <img src={require('@site/static/img/home/create-app.png').default} />
-              <h3>Contribute</h3>
+              <h3 className="flex items-center gap-2">
+                <ContributeIcon />Contribute
+              </h3>
               <p>Upload, build, and share <a href={`${portal}/apps`}>apps</a> for AI at the edge</p>
             </a>
             <a className="card" href={`${portal}/jobs`}>
               <img src={require('@site/static/img/home/circuit-board.jpg').default} />
-              <h3>Run jobs</h3>
+              <h3 className="flex items-center gap-2">
+                <RunIcon />Run jobs
+              </h3>
               <p>Create <a href={`${portal}/create-job?tab=editor&start_with_sample=true`}>science goals</a> to run apps on nodes<br/></p>
             </a>
             <a href={`${portal}/data`} className="card">
               <img src={require('@site/static/img/home/browse.png').default} />
-              <h3>Browse</h3>
+              <h3 className="flex items-center gap-2">
+                <BrowseIcon />Browse
+              </h3>
               <p>Browse <a href={`${portal}/data`}>data</a> from sensors and edge apps</p>
             </a>
             <Link to="docs/tutorials/accessing-data" className="card">
               <img src={require('@site/static/img/home/wildfire.jpg').default} />
-              <h3>Analyze</h3>
+              <h3 className="flex items-center gap-2">
+                <AnalyzeIcon/>Analyze
+              </h3>
               <p>Use Sage APIs to fetch, analyze, or integrate data</p>
             </Link>
           </div>
@@ -170,8 +188,8 @@ export default function Home() {
         <Section className="flex flex-col md:flex-row gap-2">
           <div className="bg-white shadow-sm rounded-xl md:w-2/3 h-[620px] overflow-y-scroll">
             <Link to="/news" className="hover:decoration-black">
-              <h2 className="p-4 pb-3 m-0 flex items-center">
-                <NewsIcon />&nbsp;News
+              <h2 className="flex items-center gap-2 p-4 pb-3 m-0">
+                <NewsIcon />News
               </h2>
             </Link>
             <hr className="m-0"/>
@@ -180,8 +198,8 @@ export default function Home() {
             </div>
           </div>
           <div className="bg-white shadow-sm rounded-xl md:w-1/3 h-[620px]">
-            <h2 className="p-4 pb-3 m-0 flex justify-between items-center">
-              Events
+            <h2 className="flex items-center gap-2 p-4 pb-3 m-0">
+              <EventsIcon />Events
             </h2>
             <hr className="m-0"/>
             <div className="px-4 py-8">
@@ -193,7 +211,9 @@ export default function Home() {
 
       <div className="bg-emerald-50 border-solid border-t-2 border-b-4 border-slate-200">
         <Section className="gap-2">
-          <h2 className="text-purple font-bold mb-10">Developer Friendly Tools for Research and Analysis</h2>
+          <h2 className="flex items-center gap-2 text-purple font-bold mb-10">
+            <DevIcon />Developer Tools for Research and Analysis
+          </h2>
 
           <div className="flex flex-col md:flex-row text-slate-200 gap-10">
             <div className="sci-items flex flex-col gap-4 md:w-7/12">
@@ -260,7 +280,9 @@ export default function Home() {
 
       <div className="bg-purple">
         <Section className="items-center gap-2">
-          <h2 className="text-slate-200 font-bold mb-10">Featured Science</h2>
+          <h2 className="flex items-center gap-2 text-slate-200 font-bold mb-10">
+            <SciIcon />Featured Science
+          </h2>
           <div className="flex flex-col md:flex-row text-slate-200">
             <div className="md:w-1/3 hidden md:flex mr-5 flex justify-center" >
               {getRecentPubs()
